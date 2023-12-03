62ce4aeb001d31024bcb005a145fe321526fb265
thiebes <joseph@thiebes.org>
2023-12-03 00:24:51 -0700

    added run_dice.py

---
+++ run_dice.py
@@ -0,0 +1,15 @@
+import argparse
+import dice
+
+def main():
+    parser = argparse.ArgumentParser(description="Run the dice module.")
+    parser.add_argument("filename", help="Filename of the parameters file")
+    args = parser.parse_args()
+
+    # Run the dice_runner function, results are saved in a file within the function
+    dice.dice_runner(args.filename)
+
+    print("The script has been executed. Please check the output file for results.")
+
+if __name__ == "__main__":
+    main()
