c1506415a0ca6123c188a18d5c08d76d115daa98
Joseph J. Thiebes <joseph@thiebes.org>
2024-02-13 10:47:21 -0700

    Update dice_web_app.py
    Signed-off-by: Joseph J. Thiebes <joseph@thiebes.org>

--- dice_web_app.py
+++ dice_web_app.py
@@ -1,38 +1,3 @@
-#########################################################################
-# Diffusion Insight Computation Engine (DICE) simulates optical         #
-# measures of diffusion in optoelectronic semiconducting materials      #
-# using experimental parameters, and evaluates the precision of         #
-# composite fitting methods of estimating the diffusion coefficient.    #
-#                                                                       #
-# Copyright (C) 2023-2024 Joseph J. Thiebes                             #
-#                                                                       #
-# This material is based upon work supported by the National Science    #
-# Foundation under Grant No. 2154448. Any opinions, findings, and       #
-# conclusions or recommendations expressed in this material are those   #
-# of the author(s) and do not necessarily reflect the views of the      #
-# National Science Foundation.                                          #
-#                                                                       #
-# This work is licensed under the Creative Commons Attribution 4.0      #
-# International License. To view a copy of this license, visit          #
-# http://creativecommons.org/licenses/by/4.0/ or send a letter to       #
-# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.          #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  #
-#                                                                       #
-# You should include a copy of the license or a link to it with         #
-# every copy of the work you distribute. You can do this by             #
-# including a link to the license in your README.md file or             #
-# documentation.                                                        #
-#########################################################################
-
-# This is a web app to serve the DICE program, using Flask.
-#
-# Presently it only uses the DICE function to estimate CNR from an 
-# excited state population profile as a list of comma-separated values.
-# The app uses the Flask framework to serve the function to web users.
-
 from flask import Flask, request, render_template_string
 from dice import fft_cnr
 
@@ -44,15 +9,56 @@ def home():
         values = request.form['values']
         values_list = list(map(float, values.split(',')))
         result = fft_cnr(values_list)  # Use the imported function
-        return render_template_string('''<h1>Result: {{result}}</h1>
-                                         <a href="/">Try again</a>''', result=result)
+        return render_template_string('''
+        <div style="margin: auto; width: 50%; padding: 10px;">
+            <h3>Contrast-to-noise estimate: {{result}}</h3>
+            <p><a href="/">Try again</a></p>
+            <p>Download the open-source code for the full DICE application here:<br />
+            <a href="https://github.com/thiebes/DICE" target="_blank">https://github.com/thiebes/DICE</a></p>
+            <p>Citation:</br>
+            <textarea rows="4" cols="50" readonly>Joseph J. Thiebes. (2023). thiebes/DICE. Zenodo. https://doi.org/10.5281/zenodo.10258191</textarea></p>
+            <p>This is based on a paper that is currently in peer review.</p>
+        </div>
+        ''', result=result)
     return '''
-        <form method="post">
-            Comma-separated values: <input type="text" name="values">
-            <input type="submit" value="Calculate">
-        </form>
+        <div style="margin: auto; width: 50%; padding: 10px;">
+            <form method="post">
+                <h1>Estimate Contrast-to-Noise Ratio (CNR)</h1>
+                <p>Use this application to estimate the contrast-to-noise ratio
+                from a list of comma-separated values representing a one-dimensional
+                excited state population profile.</p>
+
+                <p><em><strong>You must first subtract your background (baseline), and
+                then normalize so the peak heinght is set to unity, before
+                using this tool.</strong></em></p>
+
+                <p>Enter your profile as a list of comma-separated values: <input type="text" name="values">
+                <input type="submit" value="Calculate"></p>
+
+                <p>The CNR estimation algorithm works as follows:</p>
+
+                <ol>
+                    <li>The data undergoes a unitary (scaled by <i>n</i>^-1/2), single sided, one-dimensional discrete Fourier transform (FT).
+                    <ul>
+                        <li>See <a href="https://numpy.org/doc/stable/reference/generated/numpy.fft.rfft.html" target="_blank">One-dimensional Discrete Fourier Transform (numpy.fft.rfft)</a>.</li>
+                        <li>Within this transformed data set, the signal is positioned at zero frequency, whereas the noise predominantly occupies higher spatial frequencies.</li>
+                    </ul>
+                    <li>The modulus of each complex value in the transformed data is calculated.</li>
+                    <li>The first local minimum subsequent to the signal peak is found.</li>
+                    <li>The noise level is determined by calculating the root mean squared (RMS) amplitude for frequencies above the initial local minimum</li>
+                    <li>Since the normalized peak amplitude was unity, the CNR is simply the reciprocal of the noise level.</li>
+                    <li>The estimate is rounded to the nearest integer value.</li>
+                </ol>
+
+                <p>Download the open-source code for the full DICE application here:<br />
+                <a href="https://github.com/thiebes/DICE" target="_blank">https://github.com/thiebes/DICE</a></p>
+                <p>Citation:</br>
+                <textarea rows="4" cols="50" readonly>Joseph J. Thiebes. (2023). thiebes/DICE. Zenodo. https://doi.org/10.5281/zenodo.10258191</textarea></p>
+                <p>This is based on a paper that is currently in peer review.</p>
+                <p>Joseph Thiebes: <a href="http://thiebes.org" target="_blank">Website</a> | <a href="https://www.linkedin.com/in/thiebes/" target="_blank">LinkedIn</a> | <a href="https://www.buymeacoffee.com/leufebkxkn" target="_blank">Buy me a coffee</a>
+            </form>
+        </div>
     '''
 
 if __name__ == '__main__':
     app.run(debug=True)
-
